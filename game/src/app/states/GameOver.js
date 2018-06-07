6c18e2ea2e244c675f4ead61e58d4a708fd8d872
Francesca Colombo <francescarcolombo@gmail.com>
2018-06-07 00:29:10 -0700

    gameover screen

--- game/src/app/states/GameOver.js
+++ game/src/app/states/GameOver.js
@@ -12,9 +12,9 @@ export default class GameOver extends Phaser.State {
     let style = { font: 'bold 20px Arial', fill: '#fff', boundsAlignH: 'center', boundsAlignV: 'middle', wordWrap: true, wordWrapWidth: 400 };
     this.text = this.add.text(this.world.centerX, this.world.centerY, 'You\'re out of fuel! Unfortunately, after running into so many obstacles, ' +
       'you have to exit your career in technology. You\'re among the 52% of women who leave technology as an industry ' +
-      'because they’re made to feel unwelcome and undervalued, and see their careers stagnating. Press the spacebar to try again from the beginning', style);
+      'because they’re made to feel unwelcome and undervalued, and see their careers stagnating.', style);
     this.text.anchor.set(0.5,0.5);
-    this.continue = this.add.text(this.world.centerX, this.world.centerY + 100, 'Press spacebar to continue!', style);
+    this.continue = this.add.text(this.world.centerX, this.world.centerY + 300, 'Press spacebar to try again!', style);
     this.continue.anchor.set(0.5,0.5);
   }
 
